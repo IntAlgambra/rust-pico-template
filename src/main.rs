8af5b1e37eb336fdeacd4383ca4ba8e5e6c4685f
intalgambra <bass.valle@gmail.com>
2023-09-18 21:00:27 +0400

    fixes in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+// macros for creating entry point for function
+use rp_pico::entry;
+
+
+// we need panic handler (from 3rd party)
+use panic_halt as _;
+
+
+#[entry]
+fn main() -> ! {
+    loop {}
 }
